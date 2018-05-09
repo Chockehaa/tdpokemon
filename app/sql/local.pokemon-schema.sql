4f986a9ebb87cbd15e6b2fdcc3b4127d7aca505f
Chockehaa <imtdkso@gmail.com>
2018-05-09 19:25:38 +0200

    mon site

---
+++ app/sql/local.pokemon-schema.sql
@@ -0,0 +1,9 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+
+CREATE TABLE `pokemon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=152 DEFAULT CHARSET=latin1;
